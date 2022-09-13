65b9db4180bbccf7433d7e23c815c4bd3907e13d
bhavan <bhavan.reddy1997@gmail.com>
2022-09-13 19:00:27 -0400

    added customer-home, header

--- ninocutz-frontend/src/app/features/customer/customer.module.ts
+++ ninocutz-frontend/src/app/features/customer/customer.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CustomerHomeComponent } from './customer-home/customer-home.component';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [CustomerHomeComponent],
+  declarations: [CustomerHomeComponent, HeaderComponent],
   imports: [CustomerRoutingModule, CommonModule, SharedModule],
 })
 export class CustomerModule {}
--- ninocutz-frontend/src/app/shared/shared.module.ts
+++ ninocutz-frontend/src/app/shared/shared.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [FooterComponent],
   imports: [CommonModule, FormsModule, InputTextModule],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-    FormsModule,
-    InputTextModule,
-    ButtonModule,
-  ],
+  exports: [FooterComponent, FormsModule, InputTextModule, ButtonModule],
 })
 export class SharedModule {}
