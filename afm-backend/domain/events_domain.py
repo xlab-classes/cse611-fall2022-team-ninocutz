ab1aeacd889be77dd93e348b3da831a727b927e1
bhavan <bhavan.reddy1997@gmail.com>
2022-09-30 16:43:02 -0400

    add end points for creating current event

--- afm-backend/domain/events_domain.py
+++
@@ -1,6 +0,0 @@
-from repository import eventsRepository as eventsRepo
-
-def createFutureEvent(image_id, event_type, longitude, latitude, address, event_date,zip_code,message):
-    event_id = eventsRepo.get_event_id(event_type)
-    created_id = eventsRepo.createFutureEvent(image_id, event_id, longitude, latitude, address, event_date, zip_code, message)
-    return created_id
--- afm-backend/repository/eventsRepository.py
+++ afm-backend/repository/eventsRepository.py
@@ -3,7 +3,7 @@ from app import Database
 db = Database()
 #Future Events
 
-def createFutureEvent(image_id, event_id, longitude, latitude, address, str_now,zip_code,message):
+def createFutureEvent(image_id, event_type_id, longitude, latitude, address, str_now, zip_code, message):
 
     sql = "INSERT INTO Event (imageId, \
         eventTypeId, \
@@ -16,12 +16,12 @@ def createFutureEvent(image_id, event_id, longitude, latitude, address, str_now,
 
 
     cursor = db.cursor()
-    cursor.execute(sql, (image_id, event_id, longitude, latitude, address, str_now, zip_code, message))
+    cursor.execute(sql, (image_id, event_type_id, longitude, latitude, address, str_now, zip_code, message))
     cursor.close()
     db.commit()
     return cursor.lastrowid
 
-def get_event_id(event_type):
+def getEventTypeId(event_type):
     cursor = db.cursor()
     sql = "SELECT id from EventType where name = '" + str(event_type).strip() + "'"
     cursor.execute(sql)
@@ -29,7 +29,7 @@ def get_event_id(event_type):
     cursor.close()
     return results[0][0]
 
-def get_all_events():
+def getAllEvents():
     cursor = db.cursor()
     sql = "SELECT E.Id, E.Longitude, E.Latitude, E.Address, E.EventDate, E.EventTImeSlot, E.Zipcode, E.Message, I.Url " + \
         "FROM Event AS E INNER JOIN Image AS I ON E.ImageId = I.Id FROM Event"
@@ -38,19 +38,19 @@ def get_all_events():
     cursor.close()
     return results[0][0]
 
-def get_event_by_id(id):
+def getEventById(id):
     cursor = db.cursor()
     sql = "SELECT E.Id, E.Longitude, E.Latitude, E.Address, E.EventDate, E.EventTImeSlot, E.Zipcode, E.Message, I.Url " + \
-        "FROM Event AS E INNER JOIN Image AS I ON E.ImageId = I.Id where Id=" + id
+        "FROM Event AS E INNER JOIN Image AS I ON E.ImageId = I.Id where E.Id=" + id
     cursor.execute(sql)
     columns = cursor.description 
     results = [{columns[index][0]:column for index, column in enumerate(value)} for value in cursor.fetchall()]
     cursor.close()
     return results
 
-def get_all_future_events():
+def getAllFutureEvents():
     cursor = db.cursor()
-    sql = "SELECT  E.Id, E.Longitude, E.Latitude, E.Address, E.EventDate, E.EventTImeSlot, E.Zipcode, E.Message, I.Url " + \
+    sql = "SELECT  E.Id, E.Longitude, E.Latitude, E.Address, E.EventDate, E.EventTimeSlot, E.Zipcode, E.Message, I.Url " + \
         "FROM Event AS E INNER JOIN Image AS I ON E.ImageId = I.Id where E.EventDate > CURDATE();"
     cursor.execute(sql)
     # results = cursor.fetchall()
@@ -59,3 +59,23 @@ def get_all_future_events():
 
     cursor.close()
     return results
+
+def getCurrentEvent():
+    cursor = db.cursor()
+    sql = "SELECT E.Id, E.Longitude, E.Latitude, E.Address, E.EventDate, E.EventTimeSlot, E.Zipcode, E.Message " + \
+        "FROM CurrentEvent AS C INNER JOIN Event AS E ON C.EventId = E.Id"
+    cursor.execute(sql)
+    columns = cursor.description 
+    results = [{columns[index][0]:column for index, column in enumerate(value)} for value in cursor.fetchall()]
+
+    cursor.close()
+    return results
+
+def addCurrentEvent(eventId):
+    sql = "INSERT INTO CurrentEvent (eventId) VALUES (%s)"
+
+    cursor = db.cursor()
+    cursor.execute(sql, (eventId))
+    cursor.close()
+    db.commit()
+    return cursor.lastrowid
