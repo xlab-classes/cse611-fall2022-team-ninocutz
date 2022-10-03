a51c1c14c9625823ec10e0e98b5f321727a8c012
noterudite <deepak.dt06@gmail.com>
2022-10-03 18:21:20 -0400

    adding reset/change password

--- afm-backend/controller/userController.py
+++ afm-backend/controller/userController.py
@@ -1,8 +1,9 @@
 import datetime
 from flask import request, jsonify
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 from flask import request
 from flask import Blueprint
+from importlib_metadata import method_cache
 from app import Database
 user_blueprint = Blueprint('user_blueprint', __name__)
 
@@ -23,3 +24,24 @@ def login():
     access_token = create_access_token(identity=username, expires_delta=datetime.timedelta(minutes=60))
     db = None
     return jsonify(access_token=access_token)
+
+@user_blueprint.route("/reset_password", methods=["PUT"])
+@jwt_required()
+def reset_password():
+    db = Database()
+    cursor = db.cursor()
+    username = get_jwt_identity()
+    password = request.json.get("password", None)
+    confirm_password = request.json.get("confirm_password", None)
+    if password != confirm_password:
+        return "Passwords do not match", 401
+    sql = "SELECT * from RV_User where EmailId = %s"
+    cursor.execute(sql, (username))
+    res = cursor.fetchall()
+    if len(res) != 1:
+        return 'User does not exist', 401
+    else:
+        sql = "update RV_User set Password = %s where EmailID = %s"
+        cursor.execute(sql, (password, username))
+        db.commit()
+    return 'Password changed succesfully', 200
