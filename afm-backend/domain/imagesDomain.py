aafe452e61823f87d076efa887aa46d764d63028
bhavan <bhavan.reddy1997@gmail.com>
2022-10-17 12:47:49 -0400

    timestamp added for image upload

--- afm-backend/domain/imagesDomain.py
+++ afm-backend/domain/imagesDomain.py
@@ -1,12 +1,16 @@
 import os
 import boto3
+import time
 from botocore.exceptions import NoCredentialsError
+from werkzeug.utils import secure_filename
 from repository import imagesRepository
 
 ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
 SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+IMAGE_LOCATION = os.environ.get("IMAGE_LOCATION")
+S3_BUCKET = os.environ.get("S3_BUCKET")
 
 
 def add_image(image_type, url, userId):
@@ -14,13 +18,25 @@ def add_image(image_type, url, userId):
     return image_id
 
 
-def upload_to_aws(local_file, bucket, s3_file):
+def saveImageAndUploadToAws(file):
+    fileName = secure_filename(file.filename)
+
+    fileName = str(time.time_ns()) + fileName
+
+    if not get_file(file, IMAGE_LOCATION, fileName):
+        return 'Invalid file', 400
+
+    if not upload_to_aws(IMAGE_LOCATION + "/" + fileName, fileName):
+        return 'Image upload failed', 500
+
+    return f"https://{S3_BUCKET}.s3.amazonaws.com/{fileName}"
+
+
+def upload_to_aws(local_file, fileName):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
                       aws_secret_access_key=SECRET_KEY)
     try:
-        s3.upload_file(local_file, bucket, s3_file)
-        url = f"https://{bucket}.s3.amazonaws.com/{s3_file}"
-        print("Upload Successful", url)
+        s3.upload_file(local_file, S3_BUCKET, fileName)
         return True
     except FileNotFoundError:
         print("The file was not found")
