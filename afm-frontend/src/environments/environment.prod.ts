1bac9460c68c7462c5a191993ff90817759a60b9
noterudite <deepak.dt06@gmail.com>
2022-11-14 18:57:50 -0500

    deployment changes

--- afm-frontend/src/environments/environment.prod.ts
+++ afm-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  // Deployment specific changes
+  apibaseUrl: '/api/',
+  squareUrl:
+    'https://square.site/book/1G35M2ZHB258R/architect-for-men-buffalo-ny',
 };
