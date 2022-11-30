7c7b95ed685ab8531769a1a6071f3fb768b9eda4
bhavan <bhavan.reddy1997@gmail.com>
2022-11-30 17:29:20 -0500

    fix email display for users page

--- afm-frontend/cypress/e2e/user-home-page.cy.js
+++ afm-frontend/cypress/e2e/user-home-page.cy.js
@@ -3,24 +3,29 @@ describe("User Page", function () {
     cy.visit("https://architectformobilehairdesign.com/");
     cy.title().should("eq", "AFM-HD");
   });
+
   it("Verify Future Events", function () {
     cy.visit("https://architectformobilehairdesign.com/");
     cy.contains("Future Events");
   });
+
   it("Verify Past Events", function () {
     cy.visit("https://architectformobilehairdesign.com/");
     cy.contains("Past Events");
   });
+
   it("Verify Gallery", function () {
     cy.visit("https://architectformobilehairdesign.com/");
     cy.contains("Gallery");
-  });  
+  });
+
   it("Verify Request RV", function () {
     cy.visit("https://architectformobilehairdesign.com/");
     cy.contains("Request RV");
-  });  
+  });
+
   it("Verify Sign Up", function () {
     cy.visit("https://architectformobilehairdesign.com/");
     cy.contains("Sign Up");
-  });   
+  });
 });
