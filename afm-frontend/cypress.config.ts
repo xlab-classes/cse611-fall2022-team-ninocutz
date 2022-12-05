2e3cfc289121440b754d3f966512a3da1939def9
bhavan <bhavan.reddy1997@gmail.com>
2022-12-04 20:30:39 -0500

    added cypress settings

--- afm-frontend/cypress.config.ts
+++ afm-frontend/cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: false,
+  videosFolder: './cypress/videos',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
